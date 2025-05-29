de2c058ba0f21db07873926280b1feba3a5ed3a4
Anders0nlima <andersoncon85@gmail.com>
2025-05-29 20:36:13 -0300

    navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
+import { HomeNavBar } from "../components/reused/HomeNavBar"
+import { LanguageProvider } from "../components/translationComponents/LanguageContext"
 import styles from "../styles/pagesStyles/HomePage.module.css"
 
 export const HomePage = () => {
     return(
         <>
-        <h1 className={styles.texte}>homepage</h1>
+        <LanguageProvider>
+          <HomeNavBar/>
+        </LanguageProvider>
+        
         </>
     )
 }
