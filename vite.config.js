c5d3e32b2953379b89191ad5a75eb016e458ba09
Anders0nlima <andersoncon85@gmail.com>
2025-07-04 10:43:27 -0300

    mobile - suporte

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TC-cine"
+  base: "/TC-cine",
+  server: {
+    host: true,
+    port: 5173
+  }
 })
