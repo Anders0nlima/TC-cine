06e5e204c50e00799a5301438d52134e19d16028
Anders0nlima <andersoncon85@gmail.com>
2025-07-26 07:47:27 -0300

    git e vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Detecta se está publicando no GitHub Pages
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TC-cine",
+  base: isGitHubPages ? '/TC-cine/' : '/', // Para GitHub usa /teste-vercel/, para Vercel usa /
   server: {
     host: true,
     port: 5173
