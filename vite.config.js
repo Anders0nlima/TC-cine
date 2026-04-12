5991492966a0fe0746d9019a4e7a433bbcbc9a7c
Anders0nlima <andersoncon85@gmail.com>
2026-04-12 15:54:57 -0300

    mudanca na config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-/* global process */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Detecta se está publicando no GitHub Pages
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+// Detecta o ambiente
+const isProd = process.env.NODE_ENV === 'production';
+const isVercel = !!process.env.VERCEL;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubPages ? '/TC-cine/' : '/', // Para GitHub usa /teste-vercel/, para Vercel usa /
+  // Usa '/TC-cine/' apenas em build de produção para GitHub (não Vercel).
+  // No desenvolvimento local ou na Vercel, usa '/'
+  base: (isProd && !isVercel) ? '/TC-cine/' : '/',
   server: {
     host: true,
     port: 5173
